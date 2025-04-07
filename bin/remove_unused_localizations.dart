64c52bb718b32c5afd107fbb11b3347d180a0e01
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-04-07 14:35:55 +0300

    Improvement

--- bin/remove_unused_localizations.dart
+++ bin/remove_unused_localizations.dart
@@ -5,10 +5,10 @@ import 'package:remove_unused_localizations/remove_unused_localizations.dart';
 void main(List<String> arguments) {
   bool keepUnused = arguments.contains('--keep-unused');
 
-  print('Running Localization Cleaner...');
+  log('Running Localization Cleaner...');
   runLocalizationCleaner(keepUnused: keepUnused);
   if (keepUnused) {
-    print('✅ Unused keys saved to unused_localization_keys.txt');
+    log('✅ Unused keys saved to unused_localization_keys.txt');
   }
-  print('Done.');
+  log('Done.');
 }
